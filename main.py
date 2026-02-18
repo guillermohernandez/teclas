467c1ac4bb93d270e76229ecec5e71677bd06606
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-02-17 22:51:59 -0600

    started with function to display the file

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import sys
-from pathlib import Path
 
- 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_file>")
+        print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-
+    
     file_path = sys.argv[1]
-    file_content = open_file(file_path)  
-    print(file_content)  # Display the content
+    file_content = get_book_text(file_path)
+    print(file_content)
+    
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        book_contents = f.read()
+        return book_contents
 
 
-def open_file(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            file_contents = f.read()
-            return file_contents
-
-    except UnicodeDecodeError:
-        # error handling
-        print(f"check for utf8 error")
-        with open(file_path, "r", encoding="utf-8", errors="replace") as f:
-            file_contents = f.read()
-            return file_contents
-
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
-        sys.exit(1)
-
- 
-if __name__ == "__main__":
-    main()
+main()
