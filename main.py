aa6ad82cb3a0cf8710baae9d74a8cca4ed838b18
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-02-18 20:48:21 -0600

    renamed some variables, this is based on my bookbot

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import os
 import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <path_to_file>")
         sys.exit(1)
     
     file_path = sys.argv[1]
-    file_content = get_book_text(file_path)
+    file_content = read_file_text(file_path)
+    file_name = os.path.basename(file_path)
+    print(f"opening {file_name}...")
     print(file_content)
     
 
-def get_book_text(file_path):
+def read_file_text(file_path):
     with open(file_path) as f:
-        book_contents = f.read()
-        return book_contents
+        file_contents = f.read()
+        return file_contents
+
+def update_file_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
 
 
 main()
